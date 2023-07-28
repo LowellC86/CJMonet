baaec76096d115215ac41c6def5465233e58daa4
LowellC86 <lowellcaraballo@gmail.com>
2023-07-27 20:13:50 -0400

    merch axios

--- cjmonet/src/components/Merch.jsx
+++ cjmonet/src/components/Merch.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Merch = () => {
+  const [merchData, setMerchData] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = 'https://cj-api-production.up.railway.app/artists/';
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setMerchData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h3>Merch</h3>
+      {merchData.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {merchData.map((item) => (
+            <li key={item.id}>
+              <h4>{item.name}</h4>
+              <p>{item.description}</p>
+              <p>Price: ${item.price}</p>
+              <button>Buy Now</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
